1695b2244be584f7c4213a5152d61d77d1502779
Stanislas-silva <33936185+Stanislas-silva@users.noreply.github.com>
2017-11-23 11:36:21 -0500

    add src

---
+++ src/main.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main(int argc, int* argv[])
+{
+	float** matrice_data = (float**)malloc(sizeof(float*) * NB_LIGNE_MAX);
+	char** title_data = (char**)malloc(sizeof(char*)*NB_ARGUMENTS_MAX);
+	int nb_ligne, nb_critere;
+	open_data(matrice_data, title_data, &nb_ligne, &nb_critere);
+
+	for (int i = 0; i < nb_critere; i++)
+	{
+
+		printf("%s->%f ", title_data[i], matrice_data[3][i]);
+	}
+	
+
+	return 0;
+}
+
+
+
+int open_data(float** matrice_data, char** title_data, int* nb_ligne, int* nb_critere) {
+	FILE* file = NULL;
+	file = fopen(DATASET_FOLDER, "r");
+	if (file != NULL) {
+		char txt_buf[500];
+		fgets(txt_buf, 500, file);
+		*nb_critere = split_char(txt_buf, title_data, ',');
+
+		
+		int i = 0;
+		while (fgets(txt_buf, 500, file) != NULL)
+		{
+			matrice_data[i] = (float*)malloc(sizeof(float)*(*nb_critere));
+			split_char_tofloat(txt_buf, matrice_data[i], ',');
+			i++;
+		}
+		*nb_ligne = i;
+	}
+	fclose(file);
+	
+	return 0;
+	
+}
+
+int split_char(char* txt_buf, char** txt_split, char separateur) {
+	int sizeof_split = 0;
+	for (int i = 0; txt_buf[i] != '\n'; i++) if (txt_buf[i] == separateur) sizeof_split++;
+		
+	for (int i = 0; i < sizeof_split + 1; i++) {
+		txt_split[i] = (char*)malloc(sizeof(char)*MAXSIZE_TITLE);
+	}
+	int i = 0, j = 0, k = 0;
+	while (txt_buf[j] != '\n') {
+		if (txt_buf[j] == separateur) {
+			txt_split[i][k] = '\0';
+			i++;
+			k = 0;
+		}
+		else {
+			txt_split[i][k] = txt_buf[j];
+			k++;
+		}
+		j++;
+	}
+	txt_split[i][k] = '\0';
+	return sizeof_split + 1;
+}
+
+int split_char_tofloat(char* txt_buf, float* float_split, char separateur) {
+	
+	char tmp[30];
+
+	int i = 0, j = 0, k = 0;
+	while (txt_buf[j] != '\n' ) {
+		if (txt_buf[j] == separateur) {
+			tmp[k] = EOF;
+			float_split[i] = atof(tmp);
+			i++;
+			k = 0;
+		}
+		else {
+			tmp[k] = txt_buf[j];
+			k++;
+		}
+		j++;
+	}
+	tmp[k] = EOF;
+	float_split[i] = atof(tmp);
+	return i;
+	
+}
---
+++ src/main.h
@@ -0,0 +1,15 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#define DATASET_FOLDER "D:\\OneDrive\\Documents\\Canada\\labo\\LOG635\\labo\\lab 3\\Dataset.csv"
+#define MAXSIZE_TITLE 50
+#define NB_LIGNE_MAX 4000
+#define NB_ARGUMENTS_MAX 30
+
+
+
+
+open_data(float** matrice_data, char** title_data, int* nb_ligne, int* nb_critere);
+int split_char(char* txt_buf, char** txt_split, char separateur);
+int split_char_tofloat(char* txt_buf, float* float_split, char separateur);
+
